0cbad9d75036fe6b7b7e3bc252507ca4eebaaf21
sghong-hutom <sghong@hutom.io>
2020-10-21 17:58:21 +0900

    charades csv

--- util_scripts/_charades_generate_csv.py
+++ util_scripts/_charades_generate_csv.py
@@ -6,38 +6,116 @@ To this end, read CSV and count segments are needed beforehand.
 """
 
 
-path = '../../../../raid/jester/'
+path = '../../../../raid/Charades/'
 
+import math
 import csv 
 import os
 
-# row : ['34870', 'Drumming Fingers']
+
+# create mapping dictionary
+num2ClassName = {}
+with open(path + 'Charades_v1_classes.txt', newline='') as f:
+    tmp = f.readline()
+    while tmp:
+        num2ClassName[tmp[0:4]] = tmp[5:-1]
+        tmp = f.readline()
+
 train_data = []
-
-with open(path + 'jester-v1-train.csv', newline='') as csvfile:
-    train_reader = csv.reader(csvfile, delimiter=';', quotechar='|')
+# need to convert sec to frame number (24 fps)
+with open(path + 'Charades_v1_train.csv', newline='') as csvfile:
+    train_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in train_reader:
-        train_data.append(row)
+        if row[0] == 'id':
+            continue
+        #eg. 46GP8 ['c092 11.90 21.20', 'c147 0.00 12.60'] 597
+        id = row[0]
+        classes = row[-2].split(';')
+        fnum = len(os.listdir(path + 'cut/' + id))
 
-with open('jester_trainingSet.csv', 'w', newline='') as csvfile:
+        # extract range
+        for clip in classes:
+            if len(clip) == 0: continue
+            clip = clip.split(' ')
+            start = math.floor(float(clip[1]) * 24)
+            end = math.floor(float(clip[2]) * 24)
+
+            # condition 1 : end should be less than fnum (remove the clip)
+            if end >= fnum:
+                end = fnum
+            # condition 2 : remove the clip if len is less than 30
+            if (end - start) <= 30:
+                continue
+            if start >= fnum:
+                continue
+            train_data.append([id, num2ClassName[clip[0]], start, end-1])       
+
+with open('charades_trainingSet.csv', 'w', newline='') as csvfile:
     train_writer = csv.writer(csvfile, delimiter=',',
                     quotechar='|', quoting=csv.QUOTE_MINIMAL)
     for i in range(len(train_data)):
-        name = train_data[i][0]
-        tmp = os.listdir(path + '20bn-jester-v1/' + name)
-        train_writer.writerow([name, train_data[i][1], len(tmp)])
+        train_writer.writerow(train_data[i])
 
+# validation
 train_data = []
-with open(path + 'jester-v1-validation.csv', newline='') as csvfile:
-    train_reader = csv.reader(csvfile, delimiter=';', quotechar='|')
+# need to convert sec to frame number (24 fps)
+with open(path + 'Charades_v1_test.csv', newline='') as csvfile:
+    train_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in train_reader:
-        train_data.append(row)
+        if row[0] == 'id':
+            continue
+        id = row[0]
+        classes = row[-2].split(';')
+        fnum = len(os.listdir(path + 'cut/' + id))
 
-with open('jester_validationSet.csv', 'w', newline='') as csvfile:
+        # extract range
+        for clip in classes:
+            if len(clip) == 0: continue
+            clip = clip.split(' ')
+            start = math.floor(float(clip[1]) * 24)
+            end = math.floor(float(clip[2]) * 24)
+
+            # condition 1 : end should be less than fnum (remove the clip)
+            if end >= fnum:
+                end = fnum
+            # condition 2 : remove the clip if len is less than 30
+            if (end - start) <= 30:
+                continue
+            if start >= fnum:
+                continue
+            train_data.append([id, num2ClassName[clip[0]], start, end-1])       
+
+with open('charades_validationSet.csv', 'w', newline='') as csvfile:
     train_writer = csv.writer(csvfile, delimiter=',',
                     quotechar='|', quoting=csv.QUOTE_MINIMAL)
     for i in range(len(train_data)):
-        name = train_data[i][0]
-        tmp = os.listdir(path + '20bn-jester-v1/' + name)
-        train_writer.writerow([name, train_data[i][1], len(tmp)])
+        train_writer.writerow(train_data[i])
 
+
+# inference
+train_data = []
+
+# need to convert sec to frame number (24 fps)
+with open(path + 'Charades_v1_test.csv', newline='') as csvfile:
+    train_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in train_reader:
+        if row[0] == 'id':
+            continue
+        id = row[0]
+        classes = ''
+        fnum = len(os.listdir(path + 'cut/' + id))
+
+        # get class names
+        clips = row[-2].split(';')
+        for clip in clips:
+            if len(clip) == 0: continue 
+            if classes == '':
+                classes = num2ClassName[clip[0:4]]
+            else:
+                classes = classes + '|'+ num2ClassName[clip[0:4]]
+        train_data.append([id, classes, fnum])       
+
+with open('charades_inferenceSet.csv', 'w', newline='') as csvfile:
+    train_writer = csv.writer(csvfile, delimiter=',')
+    for i in range(len(train_data)):
+        train_writer.writerow(train_data[i])
