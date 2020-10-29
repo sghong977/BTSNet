fdad48837d469315249b6b38ce0f72581c98beab
sghong-hutom <sghong@hutom.io>
2020-10-29 15:14:33 +0900

    confirmed

--- exp_scripts/UCF_SlowFast.sh
+++ exp_scripts/UCF_SlowFast.sh
@@ -10,18 +10,24 @@ video_path='../../../raid/video_data/ucf101/ucf101_videos'
 
 model=slowfast   #resnet  sknet... 
 depth=(50)
-M=2
+M=4              #
+fuse_layer=TC    #
+ops_type=O2      #
 
-batch_size=2
+
+batch_size=32
 
 dataset=ucf101
 n_classes=101
 sample_duration=30
+
 checkpoint=100
-epoch=200
+learning_rate=0.01
+n_epochs=200
+
 
 i=0
 for i in "${!depth[@]}"; do
-    CUDA_VISIBLE_DEVICES=7 python main.py --n_epochs $epoch --M $M --root_path $root_path --annotation_path $annotation_path --video_path $video_path --sample_duration $sample_duration --result_path results  --dataset $dataset --n_classes $n_classes --ft_begin_module fc --model $model --model_depth ${depth[$i]} --batch_size $batch_size --n_threads 4 --checkpoint $checkpoint #> logs/sknet_$M$dataset$model${depth[$i]}$i.txt
+    CUDA_VISIBLE_DEVICES=7 python main.py $scheduler --learning_rate $learning_rate $resume_path $pre_path --n_epochs $n_epochs --ops_type $ops_type --fuse_layer $fuse_layer --M $M --root_path $root_path --annotation_path $annotation_path --video_path $video_path --sample_duration $sample_duration --result_path results  --dataset $dataset --n_classes $n_classes --ft_begin_module fc --model $model --model_depth ${depth[$i]} --batch_size $batch_size --n_threads 4 --checkpoint $checkpoint > logs/logs_$M$dataset$model${depth[$i]}$fuse_layer$ops_type$i.txt
 done
 #    CUDA_VISIBLE_DEVICES=4,5,6,7 python main.py --root_path $root_path --annotation_path $annotation_path --video_path $video_path --sample_duration $sample_duration --result_path results  --dataset $dataset --n_classes $n_classes ${pre_path[$i]} --ft_begin_module fc --model resnet --model_depth ${depth[$i]} --batch_size $batch_size --n_threads 4 --checkpoint 5 #> jig_tmp$i.txt
